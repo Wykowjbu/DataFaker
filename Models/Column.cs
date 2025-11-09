01465003a192e82d69969fba8332dba7e1749b1b
Wykowjbu <huynpde180519@fpt.edu.vn>
2025-11-09 18:56:53 +0700

    [dev] Init Models

---
+++ Models/Column.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataFaker.Models
+{
+    public class Column
+    {
+        public string Name { get; set; }
+        public string DataType { get; set; }
+        public bool IsNullable { get; set; }
+        public bool IsPrimaryKey { get; set; }
+        public bool IsForeignKey { get; set; }
+        public Column() { }
+    }
+}
