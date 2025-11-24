42e1d239239821d05f71b73716c49d39d0fa7f26
Wykowjbu <huynpde180519@fpt.edu.vn>
2025-11-24 22:37:02 +0700

    [Feature][Conding][connectDB] Done Connect DB and Open MainWindow

---
+++ Services/DatabaseService.cs
@@ -0,0 +1,25 @@
+﻿using DataFaker.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataFaker.Services
+{
+    public class DatabaseService
+    {
+        public async Task<(bool Success, List<Schema> Schemas)> ConnectAsync(Database database)
+        {
+            try
+            {
+                var connectDBService = new ConnectDBService(database);
+                return (true, await connectDBService.GetSchemasAsync());
+            }
+            catch (Exception)
+            {
+                return (false, new List<Schema>());
+            }
+        }
+    }
+}
---
+++ ViewModels/ConnectDBViewModel.cs
@@ -0,0 +1,66 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using DataFaker.Models;
+using DataFaker.Services;
+using System.Windows.Media;
+
+namespace DataFaker.ViewModels
+{
+    public partial class ConnectDBViewModel : ObservableObject
+    {
+        [ObservableProperty] private string databaseName;
+
+        [ObservableProperty] private string username;
+
+        [ObservableProperty] private string password;
+
+        [ObservableProperty] private string host;
+
+        [ObservableProperty] private string port;
+
+        [ObservableProperty] private string message;
+
+        [ObservableProperty] private Brush messageColor;
+
+        private readonly DatabaseService _databaseService;
+
+        public event EventHandler<List<Schema>>? ConnectionResult;  
+
+        public ConnectDBViewModel()
+        {
+            Host = "localhost";
+            Port = "5432";
+            _databaseService = new DatabaseService();
+            Connect = new AsyncRelayCommand(RunConnectAsync);
+        }
+
+        public IAsyncRelayCommand Connect { get; }
+
+        private async Task RunConnectAsync()
+        {
+            var database = new Database
+            {
+                Host = host,
+                Port = port,
+                Username = username,
+                Password = password,
+                Name = databaseName
+
+            };
+            var result = await _databaseService.ConnectAsync(database);
+            if (result.Success)
+            {
+                Message="Connection successful!";
+                MessageColor = Brushes.Green;
+                ConnectionResult?.Invoke(this, result.Schemas);
+            }
+            else
+            {
+                Message="Connection failed. Please check your credentials and try again.";
+                MessageColor = Brushes.Red;
+            }
+
+
+        }
+    }
+}
