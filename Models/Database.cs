45e0cfa3356449af00f8e40f047992bfc4b8125d
Wykowjbu <huynpde180519@fpt.edu.vn>
2025-11-24 22:38:27 +0700

    [Feature][Coding][Models] Update Database.cs(Models)

--- Models/Database.cs
+++ Models/Database.cs
@@ -10,7 +10,7 @@ namespace DataFaker.Models
     {
         public string Name { get; set; }
         public string Host { get; set; }
-        public int Port { get; set; }
+        public string Port { get; set; }
         public string Username { get; set; }
         public string Password { get; set; }
 
